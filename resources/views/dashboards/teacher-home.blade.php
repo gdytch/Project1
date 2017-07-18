d2e0c050659c32918c3eeab6b5f1c3809b4682bb
gdytch <gdytch@gmail.com>
2017-07-18 23:35:07 +0800

    mod teacher dashboard

---
+++ resources/views/dashboards/teacher-home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+@section('content')
+  <link href="{{asset('css/dashboard.css')}}" rel="stylesheet" type="text/css">
+  <div class="container-fluid">
+       <div class="row">
+         <div class="col-sm-3 col-md-2 sidebar">
+           <ul class="nav nav-sidebar">
+             <li id="overview"><a href="/admin/">Overview <span class="sr-only">(current)</span></a></li>
+             <li id="list_students"><a href="/admin/list/students">Students</a></li>
+             <li id="list_teachers"><a href="/admin/list/teachers">Teachers </a></li>
+
+           </ul>
+         </div>
+         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+           @yield('dashboard-content')
+         </div>
+       </div>
+     </div>
+@endsection
