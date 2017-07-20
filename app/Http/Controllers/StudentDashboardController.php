7491b088e6ea186621a0a9e518647591196226d3
gdytch <gdytch@gmail.com>
2017-07-21 05:35:10 +0800

    some dashboard changes

--- app/Http/Controllers/StudentDashboardController.php
+++ app/Http/Controllers/StudentDashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Auth;
 
 class StudentDashboardController extends Controller
 {
@@ -23,6 +25,7 @@ class StudentDashboardController extends Controller
      */
     public function index()
     {
-        return view('dashboards.student-home');
+        $user = Auth::user();
+        return view('dashboards.student-home')->with('user', $user);
     }
 }
--- resources/views/layouts/student.blade.php
+++ resources/views/layouts/student.blade.php
@@ -6,23 +6,11 @@
        <div class="row">
          <div class="col-sm-3 col-md-2 sidebar">
            <ul class="nav nav-sidebar">
-             <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-             <li><a href="#">Reports</a></li>
-             <li><a href="#">Analytics</a></li>
-             <li><a href="#">Export</a></li>
-           </ul>
-           <ul class="nav nav-sidebar">
-             <li><a href="">Nav item</a></li>
-             <li><a href="">Nav item again</a></li>
-             <li><a href="">One more nav</a></li>
-             <li><a href="">Another nav item</a></li>
-             <li><a href="">More navigation</a></li>
-           </ul>
-           <ul class="nav nav-sidebar">
-             <li><a href="">Nav item again</a></li>
-             <li><a href="">One more nav</a></li>
-             <li><a href="">Another nav item</a></li>
-           </ul>
+             <li><a href="#">Dashboard <span class="sr-only">(current)</span></a></li>
+             <li><a href="#">Grades</a></li>
+             <li><a href="#">Subjects</a></li>
+             <li><a href="#"></a></li>
+            </ul>
          </div>
          @yield('dashboard-content')
        </div>
